2cf3295694c3068c40b0ebbd3a6a2b341039b85b
Kovács-Bodó Csemge <h160431@stud.u-szeged.hu>
2023-11-15 23:01:16 +0100

    warehouse stock tábla feltöltve

--- webshop-rf1/src/app/pages/home/home.component.ts
+++ webshop-rf1/src/app/pages/home/home.component.ts
@@ -37,16 +37,6 @@ export class HomeComponent implements OnInit{
             this.products = products;
           });
 
-
-          const prod: Product= {
-              productId: 1,
-              productName: 'Iphone 15',
-              price: 800,
-              tags: ['Mobile', 'Apple','128 Gb'],
-              description: 'The iPhone, a flagship product from Apple, is celebrated for its elegant design and premium build quality. It runs on the iOS operating system, known for its stability and vast app ecosystem. Equipped with Retina displays, iPhones offer sharp and vibrant visuals. With powerful A13 Bionic chips (or later), they deliver fast and efficient performance. iPhones are celebrated for their high-quality cameras, Face ID security, and seamless integration into the broader Apple ecosystem, ensuring a user-friendly and technologically advanced smartphone experience.',
-              imageUrl: 'products_image/iphone_15.jpg'
-
-          }
           // this.productService.createProduct(prod)
     }
     });
