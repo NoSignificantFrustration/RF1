89e595da3c025cfd124609bd75c2830c5d5f38b9
Szilágyi Martin György <h269583@stud.u-szeged.hu>
2023-11-22 19:39:04 +0100

    fel kesz

--- webshop-rf1/src/app/pages/home/tags/tags.component.ts
+++ webshop-rf1/src/app/pages/home/tags/tags.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Tags } from 'src/app/shared/models/Tags';
+import { TagsService } from 'src/app/shared/services/tags.service';
+import { ProductService } from 'src/app/shared/services/product.service';
+import { Product } from 'src/app/shared/models/Product';
 
 @Component({
   selector: 'app-tags',
@@ -6,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./tags.component.scss']
 })
 export class TagsComponent {
+  tags : Tags [] = [];
+  products : Product [] = [];
+  constructor(private tagsService: TagsService,private productService:ProductService){
 
+  }
+  ngOnInit(): void {
+    this.tagsService.getAllTags().subscribe(tags => {
+      this.tags = tags;
+    });
+  }
+  getProductByTag(tagId: string, tagName: string) {
+    this.productService.getAllProducts().subscribe(products => {
+     
+      const filteredProducts = products.filter(product => product.tags && product.tags.includes(tagId));
+
+      filteredProducts.forEach(product => {
+        this.productService.getProductImageUrl(product.imageUrl).subscribe(url => {
+          product.imageUrl = url;
+        });
+      });
+  
+     
+      this.products = filteredProducts;
+
+    });
+    
+  }
+  
 }
