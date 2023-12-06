efe90099cfce0863e4acf2a0e71c7c7455925019
Mackovic Mark <h161221@stud.u-szeged.hu>
2023-12-06 18:25:47 +0100

    product editForm fill

--- webshop-rf1/src/app/pages/admin/customer/customer.component.ts
+++ webshop-rf1/src/app/pages/admin/customer/customer.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogModule,
+} from '@angular/material/dialog';
 import { Customer } from 'src/app/shared/models/Customer';
 import { CustomerService } from 'src/app/shared/services/customer.service';
 
@@ -10,7 +16,10 @@ import { CustomerService } from 'src/app/shared/services/customer.service';
 export class CustomerComponent implements OnInit {
   customers: Customer[] = [];
 
-  constructor(private customerService: CustomerService) {}
+  constructor(
+    private customerService: CustomerService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.customerService
@@ -21,4 +30,26 @@ export class CustomerComponent implements OnInit {
   onDelete(customerId: string) {
     this.customerService.deleteCustomerById(customerId);
   }
+
+  openDialog(customer: Customer) {
+    const dialogRef = this.dialog.open(ConfirmDialog, {
+      data: customer,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.onDelete(customer.customerId);
+      }
+    });
+  }
+}
+
+@Component({
+  selector: 'confirm-dialog',
+  templateUrl: 'confirm_dialog.html',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+})
+export class ConfirmDialog {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Customer) {}
 }
--- webshop-rf1/src/app/pages/admin/products/new-product-dialog/new-product-dialog.component.ts
+++ webshop-rf1/src/app/pages/admin/products/new-product-dialog/new-product-dialog.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Product } from 'src/app/shared/models/Product';
+import { Tags } from 'src/app/shared/models/Tags';
 import { ProductService } from 'src/app/shared/services/product.service';
 
 @Component({
@@ -11,11 +12,14 @@ import { ProductService } from 'src/app/shared/services/product.service';
 })
 export class NewProductDialogComponent implements OnInit {
   edit: string = 'default';
+  tags: Tags[] = [];
+  product: Product | null = null;
+
   productForm: FormGroup = this.fb.group({
-    productId: [null],
+    productId: [''],
     productName: ['', Validators.required],
     price: [null, Validators.required],
-    tags: this.fb.array([]),
+    tags: [[]],
     description: [''],
     imageUrl: ['products_image/zflip5.jpg', Validators.required],
   });
@@ -23,16 +27,44 @@ export class NewProductDialogComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private productService: ProductService,
-    @Inject(MAT_DIALOG_DATA) public initialedit: string
+    @Inject(MAT_DIALOG_DATA)
+    public dialogdata: {
+      initialedit: string;
+      tags: Tags[];
+      product: Product;
+    },
+    public dialog: MatDialog
   ) {
-    this.edit = initialedit;
+    this.edit = dialogdata.initialedit;
+    this.tags = dialogdata.tags;
+    this.product = dialogdata.product ? dialogdata.product : null;
+    if (dialogdata.product) {
+      this.productForm.controls['productName'].setValue(
+        dialogdata.product.productName
+      );
+
+      this.productForm.controls['productId'].setValue(
+        dialogdata.product.productId
+      );
+      this.productForm.controls['price'].setValue(dialogdata.product.price);
+
+      this.productForm.controls['tags'].setValue(dialogdata.product.tags);
+
+      this.productForm.controls['description'].setValue(
+        dialogdata.product.description
+      );
+      this.productForm.controls['imageUrl'].setValue(
+        dialogdata.product.imageUrl
+      );
+    }
   }
 
   ngOnInit(): void {}
 
   onSubmit(): void {
     const formData = this.productForm.value as Product;
-    if (!formData.productName || !formData.price) {
+    if (!formData.productName || !formData.price || this.product == null) {
+    } else if (this.product) {
     } else {
       this.productService.createProduct(formData);
     }
